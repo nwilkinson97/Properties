cf0709f77375e21fae4ea2a0bb24f5dba887956b
nwilkinson97 <nathan.b.wilkinson@gmail.com>
2019-02-25 09:32:05 -0500

    Move Person Class
    Move Person Class to separate file using Quick Actions Shortcut.  {CTRL} + .

---
+++ Properties/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+	   public DateTime BirthDate { get; set; }
+
+	   public int Age
+	   {
+		  get
+		  {
+			 var timespan = DateTime.Today - BirthDate;
+			 var years = timespan.Days / 365;
+
+			 return years;
+		  }
+	   }
+    }
+}
--- Properties/Program.cs
+++ Properties/Program.cs
@@ -1,23 +1,5 @@
-﻿using System;
-
-namespace Properties
+﻿namespace Properties
 {
-    public class Person
-    {
-	   public DateTime BirthDate { get; set; }
-
-	   public int Age
-	   {
-		  get
-		  {
-			 var timespan = DateTime.Today - BirthDate;
-			 var years = timespan.Days / 365;
-
-			 return years;
-		  }
-		  	   }
-    }
-
     class Program
     {
 

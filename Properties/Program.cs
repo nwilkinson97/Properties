041adf5a1dfe67213d3ae6cb85ba0d4396c99eb9
nwilkinson97 <nathan.b.wilkinson@gmail.com>
2019-02-25 09:30:42 -0500

    Add Age Property
    Cannot use autoproperty because we need to perform calculations before returning the Age.

--- Properties/Program.cs
+++ Properties/Program.cs
@@ -5,6 +5,17 @@ namespace Properties
     public class Person
     {
 	   public DateTime BirthDate { get; set; }
+
+	   public int Age
+	   {
+		  get
+		  {
+			 var timespan = DateTime.Today - BirthDate;
+			 var years = timespan.Days / 365;
+
+			 return years;
+		  }
+		  	   }
     }
 
     class Program

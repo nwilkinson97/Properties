487009c36cf9fbe5527e6bc8b221f263754f262d
nwilkinson97 <nathan.b.wilkinson@gmail.com>
2019-02-25 09:17:23 -0500

    Created Person Class with Property

--- Properties/Program.cs
+++ Properties/Program.cs
@@ -1,7 +1,14 @@
-﻿namespace Properties
+﻿using System;
+
+namespace Properties
 {
     class Program
     {
+	   public class Person
+	   {
+		  public DateTime BirthDate { get; set; }
+	   }
+
         static void Main(string[] args)
         {
         }

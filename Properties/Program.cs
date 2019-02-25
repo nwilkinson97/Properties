0ff753f453ee8f71e8e8bf42e4cc23f709f08025
nwilkinson97 <nathan.b.wilkinson@gmail.com>
2019-02-25 09:36:24 -0500

    Get Customer Age
    Using a new instance of the Person class set the BirthDate Property and retreive the Persons age using the getter.  Note:  We do not have a setter because this infomration is calculated based on the field BirthDate.

--- Properties/Program.cs
+++ Properties/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Properties
+﻿using System;
+
+namespace Properties
 {
     class Program
     {
-
 	   static void Main(string[] args)
 	   {
+		  var person = new Person
+		  {
+			 BirthDate = new DateTime(1997, 1, 12)
+		  };
+
+		  Console.WriteLine(person.Age);
 	   }
     }
 }
